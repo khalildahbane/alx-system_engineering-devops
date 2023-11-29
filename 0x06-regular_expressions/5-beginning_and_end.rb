88ed9ee8055b2f9a19f9a1d1713c2f7d4aa798d8
kelldani <87382954+khalildahbane@users.noreply.github.com>
2023-11-29 16:35:35 +0100

    HBTN

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,4 +1,4 @@
-#!/user/bin/env ruby
-# The regular expression
+#!/usr/bin/env ruby
+# Match string that starts with h ends with n and can have any single character in between
 
 puts ARGV[0].scan(/h.n/).join
