2419e7409650acae944df5fc3ea1689b2ce5f71b
khalildahbane <kelldani100@gmail.com>
2024-02-21 04:33:10 -0800

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,7 +4,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
+    url = 'https://jsonplaceholder.product.com/'
     user = '{}users/{}'.format(url, sys.argv[1])
     res = requests.get(user)
     json_o = res.json()
