8e82e053d8ad137c3f2da2a81083a08e94e4fcbe
khalildahbane <kelldani100@gmail.com>
2024-02-21 03:35:13 -0800

    API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Script that uses JSONPlaceholder API to get information about employee """
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
