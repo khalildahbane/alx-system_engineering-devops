63e7a8ab848b007a48dab2d1db0dc81a65b29a2e
khalildahbane <kelldani100@gmail.com>
2024-03-08 11:27:52 -0800

    advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-"""Queries the Reddit API and returns the number of subscribers"""
+    """
+    Queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit. If an
+    invalid subreddit is given, the function will return 0.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     head = {"User-Agent": "aarizat"}
     r = requests.get(url, headers=head, allow_redirects=False)
