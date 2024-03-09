e3225f8519d5a8c137807c77385106753c53a8c0
khalildahbane <kelldani100@gmail.com>
2024-03-09 04:24:41 -0800

    added

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,3 +6,23 @@ def recurse(subreddit, hot_list=[]):
 """
 queries the Reddit API and returns a list containing the titles of all
 """
+
+def recurse(subreddit, after=None):
+    """method doc"""
+    headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+    params = {"after": after}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False,
+                            headers=headers, params=params)
+    all_posts = []
+    if response.status_code == 200:
+        data = response.json()
+        after = data["data"]["after"]
+        if after is None:
+            return all_posts
+        for post in data["data"]["children"]:
+            all_posts.append(post["data"]["title"])
+        next = recurse(subreddit, after)
+        all_posts.extend(next)
+        return all_posts
+    return None
