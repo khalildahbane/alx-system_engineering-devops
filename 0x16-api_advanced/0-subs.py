e5a38be80a6cbbcb613f34415663aa27fe956178
khalildahbane <kelldani100@gmail.com>
2024-03-10 05:03:37 -0700

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,6 +12,5 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 404:
-        return data["data"]["subscribers"]
-    else:
         return 0
+    return r.json().get("data").get("subscribers")
