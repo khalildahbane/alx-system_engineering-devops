0077a5512cc2861935b6a8a1731c01520fe2f33c
khalildahbane <kelldani100@gmail.com>
2024-03-10 05:05:25 -0700

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """
-this doc for module
+Use ReditAPI
 """
 import requests
 
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
-
 
 def number_of_subscribers(subreddit):
-    """method doc"""
+    """
+    Queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit. If an
+    invalid subreddit is given, the function will return 0.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 404:
+    head = {"User-Agent": "aarizat"}
+    r = requests.get(url, headers=head, allow_redirects=False)
+    if r.status_code == 404:
         return 0
     return r.json().get("data").get("subscribers")
