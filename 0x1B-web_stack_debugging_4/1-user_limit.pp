29c8cc6d73cb77692820a31be1d12f872c505600
khalildahbane <kelldani100@gmail.com>
2024-03-13 15:54:19 -0700

    Web stack

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Enable the user holberton to login.
+
+exec { 'increase-hard-file-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
